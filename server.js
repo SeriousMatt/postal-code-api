48469966fb1031a1634d132e51fcfb90bd6b480a
SeriousMatt <mattrzr@gmail.com>
2018-04-04 17:36:16 -0400

    Combines both postgre query function and routes: state and postal

--- server.js
+++ server.js
@@ -1,20 +1,50 @@
 //Call libraries
 var express        = require('express');      
 var app            = express();                
-var bodyParser     = require('body-parser');
-var postgreData    = require('./postgre.js');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-//Set port
-var port = process.env.PORT || 8080;       
-var router = express.Router();              
-//Add route
-router.get('/', function(req, res) {
-    res.json({ postgreData });   
+//Add routes
+//Set default route
+app.get('/', (req) => {
+  req
+    .res.send('postal-code-api');
 });
-
-app.use('/api', router);
-//Output
-app.listen(port);
-console.log('Running on port: ' + port);
+//Find a single place via postal code
+app.get('/postal/:code', function (req, res) {
+	var query    = "SELECT * FROM postal_codes WHERE postal_code=" + req.params.code;
+	let response = queryPostgre(res, query);
+  res.send(response);
+});
+//Find all of the postal codes in a state
+app.get('/state/:code', function (req, res) {
+	let query = "SELECT * FROM postal_codes WHERE state_code = '"+ req.params.code +"'";
+	let response = queryPostgre(res, query);
+  res.send(response);
+});
+//Listen on port 3000
+app.listen(3000, () => {
+  console.log('Running on port 3000');
+});
+//Postgre query
+var queryPostgre = function (res, query){ 
+	console.log(query);
+	//Heroku Cli
+	//https://devcenter.heroku.com/articles/heroku-postgresql#using-the-cli
+	const { Client } = require('pg');
+	//Connection String to Cloud Database
+	const client = new Client({
+	  connectionString: 'postgres://rlujyumwdxsfhb:1b6b8ee88cb54126c5fbbcd6f5dde7d083e9e795448060ee1aed85093934a2d2@ec2-23-21-121-220.compute-1.amazonaws.com:5432/d8m1gq2dnorrtm',
+	  ssl: true,
+	});
+	//Form Connection
+	client.connect();
+	//Postgre Query
+	client.query(query, (err, res) => {
+	  if (err) throw err;
+	  let data = '';
+	  for (let row of res.rows) {
+	  	data = data + JSON.stringify(row)
+	  	// return data;
+	  	console.log(data);
+	  }
+	  client.end();
+	});
+}
