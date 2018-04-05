dfdc0f7146ff98b6b5107dceeff55d827613b9a1
SeriousMatt <mattrzr@gmail.com>
2018-04-05 17:20:15 -0400

    API now returning expected json, data folder w/ script, and fully linted

---
+++ data/postal_codes.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS postal_codes;
+CREATE DATABASE postal_codes;
+
+\c postal_codes;
+
+CREATE TABLE postal_codes(COUNTRY_CODE char(2),
+ POSTAL_CODE numeric,
+ CITY varchar,
+ STATE varchar,
+ STATE_CODE varchar,
+ COUNTY varchar, 
+ COUNTY_CODE varchar, 
+ ADMIN_NAME3 varchar,
+ ADMIN_CODE3 varchar,  
+ LATITUDE numeric, 
+ LONGITUDE numeric, 
+ ACCURACY numeric);
+
+\copy postal_codes FROM '/Users/matthewreynolds/desktop/postal-code-api/US.csv' WITH CSV;
--- server.js
+++ server.js
@@ -1,50 +1,52 @@
-//Call libraries
-var express        = require('express');      
-var app            = express();                
-//Add routes
-//Set default route
-app.get('/', (req) => {
-  req
-    .res.send('postal-code-api');
+// Call libraries
+const express = require('express');
+const app = express();
+const promise = require('bluebird');
+// Use bluebird promises
+const options = {
+  promiseLib: promise,
+};
+const pgp = require('pg-promise')(options);
+// Set for Heroku
+pgp.pg.defaults.ssl = true;
+// Abstract later for security
+const connectionString = 'postgres://rlujyumwdxsfhb:1b6b8ee88cb54126c5fbbcd6f5dde7d083e9e795448060ee1aed85093934a2d2@ec2-23-21-121-220.compute-1.amazonaws.com:5432/d8m1gq2dnorrtm';
+const db = pgp(connectionString);
+// Add routes
+app.get('/', function(req, res) {
+  req.res.send('postal-code-api');
 });
-//Find a single place via postal code
-app.get('/postal/:code', function (req, res) {
-	var query    = "SELECT * FROM postal_codes WHERE postal_code=" + req.params.code;
-	let response = queryPostgre(res, query);
-  res.send(response);
+// Find a single place via postal code
+app.get('/postal/:code', function(req, res) {
+  db.any('SELECT * FROM postal_codes WHERE postal_code=' + req.params.code)
+    .then(function(data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Pulling place by postal code',
+        });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
 });
-//Find all of the postal codes in a state
-app.get('/state/:code', function (req, res) {
-	let query = "SELECT * FROM postal_codes WHERE state_code = '"+ req.params.code +"'";
-	let response = queryPostgre(res, query);
-  res.send(response);
+// Find all of the postal codes in a state
+app.get('/state/:code/:page', function(req, res) {
+  db.any("SELECT * FROM postal_codes WHERE state_code='" + req.params.code + "' LIMIT " + req.params.page)
+    .then(function(data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Pulling postal codes for state',
+        });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
 });
-//Listen on port 3000
-app.listen(process.env.PORT || 3000, () => {
+// Listen on port 3000
+app.listen(process.env.PORT || 3000, function() {
   console.log('Running on port 3000');
 });
-//Postgre query
-var queryPostgre = function (res, query){ 
-	console.log(query);
-	//Heroku Cli
-	//https://devcenter.heroku.com/articles/heroku-postgresql#using-the-cli
-	const { Client } = require('pg');
-	//Connection String to Cloud Database
-	const client = new Client({
-	  connectionString: 'postgres://rlujyumwdxsfhb:1b6b8ee88cb54126c5fbbcd6f5dde7d083e9e795448060ee1aed85093934a2d2@ec2-23-21-121-220.compute-1.amazonaws.com:5432/d8m1gq2dnorrtm',
-	  ssl: true,
-	});
-	//Form Connection
-	client.connect();
-	//Postgre Query
-	client.query(query, (err, res) => {
-	  if (err) throw err;
-	  let data = '';
-	  for (let row of res.rows) {
-	  	data = data + JSON.stringify(row)
-	  	// return data;
-	  	console.log(data);
-	  }
-	  client.end();
-	});
-}
